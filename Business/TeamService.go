1adb18a14b156de4c64a014ae51fda346c9db71d
Dmitry Khramushin <dkhramushin@gmail.com>
2018-02-06 02:02:56 +0300

    remove closing section

--- Business/TeamService.go
+++ Business/TeamService.go
@@ -142,10 +142,6 @@ func Draw(w http.ResponseWriter, r *http.Request){
 	ResponseWithJSON(w, respBody, http.StatusOK)
 }
 
-func CloseSession(){
-	DataAccess.CloseSession()
-}
-
 func ResponseWithJSON(w http.ResponseWriter, json []byte, code int) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(code)
--- main.go
+++ main.go
@@ -5,11 +5,9 @@ import (
 	"net/http"
 	"TestProject/Route"
 	"TestProject/Settings"
-	"TestProject/Business"
 )
 
 func main() {
 	router := Route.NewRouter()
 	log.Fatal(http.ListenAndServe(Settings.ServiceAddr, router))
-	defer Business.CloseSession()
 }
